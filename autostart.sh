f53db511fe0e157220ebc22dcdbaf3578f0e5175
Ruben_&_Linux:~ <escombr@hotmail.com>
2026-02-25 09:54:24 +0100

    Add files via upload

---
+++ autostart.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+set +e
+
+# obs
+dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots >/dev/null 2>&1
+
+# notify
+swaync -c ~/.config/mango/swaync/config.jsonc -s ~/.config/mango/swaync/style.css >/dev/null 2>&1 &
+
+# Burgos Castilla y León
+wlsunset -g 0.8 -l 42.34106 -L -3.70184 & >/dev/null 2>&1 &
+
+# wallpaper
+swaybg -i /home/ruben/Imágenes/Wallpapers/china.png >/dev/null 2>&1 &
+
+# top bar
+waybar -c ~/.config/mango/waybar/config.jsonc -s ~/.config/mango/waybar/style.css >/dev/null 2>&1 &
+
+# xwayland dpi scale
+echo "Xft.dpi: 140" | xrdb -merge
+# xrdb merge ~/.Xresources >/dev/null 2>&1
+
+# ime input
+fcitx5 --replace -d >/dev/null 2>&1 &
+
+# keep clipboard content
+wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
+
+# clipboard content manager
+wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
+
+# bluetooth 
+blueman-applet >/dev/null 2>&1 &
+
+# network
+nm-applet >/dev/null 2>&1 &
+
+# Permission authentication
+/usr/lib/polkit-kde-agent/polkit-kde-authentication-agent-1 >/dev/null 2>&1 &
+
+# inhibit by audio
+sway-audio-idle-inhibit >/dev/null 2>&1 &
+
+# change light value and volume value by swayosd-client in keybind
+swayosd-server >/dev/null 2>&1 &
